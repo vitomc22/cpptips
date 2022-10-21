afc1a776ae227edeb7670cc6a3a561df94388720
vonascimento <victor.oliveira@philips.com>
2022-10-21 15:23:21 -0300

    Vector example

---
+++ vector.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+//vector é um container de array, na prática é um array com super poderes, como iteradores begin e end
+//são dinamicos e  por isso não é necessário declarar o tamanho por exemplo
+
+int main(int argc, char const *argv[]) {
+  std::vector<int> numeros;
+
+  for (int i = 0; i < 5; i++) {
+    numeros.push_back(i);
+  }
+
+  for (auto i = numeros.begin(); i != numeros.end(); ++i) {
+    std::cout << "numeros de i: " << *i << '\n';
+  }
+
+  std::cout << "Tamanho do vector numeros: " << numeros.size() << '\n';
+  std::cout << "Tamanho máximo do vector numeros: " << numeros.max_size() << '\n';
+  std::cout << "Capacidade do vector numeros: " << numeros.capacity() << '\n';
+
+  std::cout << "Limpando vector numeros... "<< '\n';
+  numeros.clear();
+
+  std::cout << "Tamanho depois de limpar o vector:  "<< numeros.size() << '\n';
+
+  return 0;
+}
